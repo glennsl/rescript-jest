109465ba0d4f2db1878271f6b1458d9fe5c51ba9
Glenn Slotte <glennsl@users.noreply.github.com>
2017-10-05 18:55:58 +0200

    Add testAll function to generate tests from a list of inputs (#12)
    * add testAll et al.
    
    * val -> external in mli
    
    * changes to js artifacts after bs-platform upgrade

--- __tests__/globals_test.ml
+++ __tests__/globals_test.ml
@@ -2,26 +2,38 @@ open Jest
 open Expect
 
 let _ =
-  test "test" (fun _ -> expect (1 + 2) |> toBe 3);
+  test "test" (fun _ ->
+    expect (1 + 2) |> toBe 3);
   (*
-  test "test - expect fail" (fun _ -> expect (1 + 2) |> toBe 4);
+  test "test - expect fail" (fun _ ->
+    expect (1 + 2) |> toBe 4);
   *)
   
-  testAsync "testAsync" (fun done_ -> done_ (expect (1 + 2) |> toBe 3));
+  testAsync "testAsync" (fun done_ ->
+    done_ (expect (1 + 2) |> toBe 3));
   (*
   testAsync "testAsync - no done" (fun _ -> ());
   *)
   (*
-  testAsync "testAsync - expect fail" (fun done_ -> done_ (expect (1 + 2) |> toBe 4));
+  testAsync "testAsync - expect fail" (fun done_ ->
+    done_ (expect (1 + 2) |> toBe 4));
   *)
   
-  testPromise "testPromise" (fun _ -> Js.Promise.resolve (expect (1 + 2) |> toBe 3));
+  testPromise "testPromise" (fun _ ->
+    Js.Promise.resolve (expect (1 + 2) |> toBe 3));
   (*
-  testPromise "testPromise - reject" (fun _ -> Promise.reject ());
+  testPromise "testPromise - reject" (fun _ ->
+    Promise.reject ());
   *)
   (*
-  testPromise "testPromise - expect fail" (fun _ -> Promise.resolve (expect (1 + 2) |> toBe 4));
+  testPromise "testPromise - expect fail" (fun _ ->
+    Promise.resolve (expect (1 + 2) |> toBe 4));
   *)
+
+  testAll "testAll" ["foo"; "bar"; "baz"] (fun input ->
+    expect (Js.String.length input) |> toEqual 3);
+  testAll "testAll - tuples" [("foo", 3); ("barbaz", 6); ("bananas!", 8)] (fun (input, output) ->
+    expect (Js.String.length input) |> toEqual output);
   
   describe "describe" (fun _ ->
     test "some aspect" (fun _ -> expect (1 + 2) |> toBe 3)
@@ -68,9 +80,16 @@ let _ =
     (*
     Only.test "Only.test" (fun _ -> (expect (1 + 2) |> toBe 3));
 
-    Only.testAsync "Only.testAsync" (fun done_ -> done_ (expect (1 + 2) |> toBe 3));
+    Only.testAsync "Only.testAsync" (fun done_ ->
+      done_ (expect (1 + 2) |> toBe 3));
 
-    Only.testPromise "Only.testPromise" (fun _ -> Promise.resolve (expect (1 + 2) |> toBe 3));
+    Only.testPromise "Only.testPromise" (fun _ ->
+      Promise.resolve (expect (1 + 2) |> toBe 3));
+
+    Only.testAll "testAll" ["foo"; "bar"; "baz"] (fun input ->
+      expect (Js.String.length input) |> toEqual 3);
+    Only.testAll "testAll - tuples" [("foo", 3); ("barbaz", 6); ("bananas!", 8)] (fun (input, output) ->
+      expect (Js.String.length input) |> toEqual output);
 
     Only.describe "Only.describe" (fun _ ->
       test "some aspect" (fun _ -> (expect (1 + 2)) |> toBe 3)
@@ -87,6 +106,11 @@ let _ =
 
     Skip.testPromise "Skip.testPromise" (fun _ ->
       Js.Promise.resolve (expect (1 + 2) |> toBe 3));
+
+    Skip.testAll "testAll" ["foo"; "bar"; "baz"] (fun input ->
+      expect (Js.String.length input) |> toEqual 3);
+    Skip.testAll "testAll - tuples" [("foo", 3); ("barbaz", 6); ("bananas!", 8)] (fun (input, output) ->
+      expect (Js.String.length input) |> toEqual output);
 
     Skip.describe "Skip.describe" (fun _ ->
       test "some aspect" (fun _ -> expect (1 + 2) |> toBe 3)
