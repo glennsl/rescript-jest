d865b4be81bf30c6294d695f228f1639ecd07242
glennsl <glenn@slotte.net>
2017-12-27 21:02:26 +0100

    replace expect in globals_test and globals_only_test with pass/fail

--- __tests__/globals_only_test.ml
+++ __tests__/globals_only_test.ml
@@ -1,21 +1,20 @@
 open Jest
-open Expect
 
 let _ =
 
-  Only.test "Only.test" (fun _ -> (expect (1 + 2) |> toBe 3));
+  Only.test "Only.test" (fun _ -> pass);
 
-  Only.testAsync "Only.testAsync" (fun done_ ->
-    done_ (expect (1 + 2) |> toBe 3));
+  Only.testAsync "Only.testAsync" (fun finish ->
+    finish pass);
 
   Only.testPromise "Only.testPromise" (fun _ ->
-    Js.Promise.resolve (expect (1 + 2) |> toBe 3));
+    Js.Promise.resolve pass);
 
   Only.testAll "testAll" ["foo"; "bar"; "baz"] (fun input ->
-    expect (Js.String.length input) |> toEqual 3);
+    if Js.String.length input == 3 then pass else fail "");
   Only.testAll "testAll - tuples" [("foo", 3); ("barbaz", 6); ("bananas!", 8)] (fun (input, output) ->
-    expect (Js.String.length input) |> toEqual output);
+    if Js.String.length input == output then pass else fail "");
 
   Only.describe "Only.describe" (fun _ ->
-    test "some aspect" (fun _ -> (expect (1 + 2)) |> toBe 3)
+    test "some aspect" (fun _ -> pass)
   );
--- __tests__/globals_test.ml
+++ __tests__/globals_test.ml
@@ -1,5 +1,4 @@
 open Jest
-open Expect
 
 let _ =
   test "pass" (fun _ ->
@@ -9,51 +8,51 @@ let _ =
     fail "");
 
   test "test" (fun _ ->
-    expect (1 + 2) |> toBe 3);
+    pass);
     
   Skip.test "test - expect fail" (fun _ ->
-    expect (1 + 2) |> toBe 4);
+    fail "");
   
-  testAsync "testAsync" (fun done_ ->
-    done_ (expect (1 + 2) |> toBe 3));
+  testAsync "testAsync" (fun finish ->
+    finish pass);
     
   Skip.testAsync "testAsync - no done" (fun _ -> ());
 
-  Skip.testAsync "testAsync - expect fail" (fun done_ ->
-    done_ (expect (1 + 2) |> toBe 4));
+  Skip.testAsync "testAsync - expect fail" (fun finish ->
+    finish (fail ""));
   
   testPromise "testPromise" (fun _ ->
-    Js.Promise.resolve (expect (1 + 2) |> toBe 3));
+    Js.Promise.resolve pass);
 
   Skip.testPromise "testPromise - reject" (fun _ ->
     Js.Promise.reject (Failure ""));
 
   Skip.testPromise "testPromise - expect fail" (fun _ ->
-    Js.Promise.resolve (expect (1 + 2) |> toBe 4));
+    Js.Promise.resolve (fail ""));
 
   testAll "testAll" ["foo"; "bar"; "baz"] (fun input ->
-    expect (Js.String.length input) |> toEqual 3);
+    if Js.String.length input == 3 then pass else fail "");
   testAll "testAll - tuples" [("foo", 3); ("barbaz", 6); ("bananas!", 8)] (fun (input, output) ->
-    expect (Js.String.length input) |> toEqual output);
+    if Js.String.length input == output then pass else fail "");
   
   describe "describe" (fun _ ->
-    test "some aspect" (fun _ -> expect (1 + 2) |> toBe 3)
+    test "some aspect" (fun _ -> pass)
   );
   
   describe "beforeAll" (fun _ -> 
     let x = ref 0 in
     
     beforeAll (fun _ -> x := !x + 4);
-    test "x is 4" (fun _ -> expect !x |> toBe 4);
-    test "x is still 4" (fun _ -> expect !x |> toBe 4);
+    test "x is 4" (fun _ -> if !x == 4 then pass else fail "");
+    test "x is still 4" (fun _ -> if !x == 4 then pass else fail "");
   );
   
   describe "beforeEach" (fun _ -> 
     let x = ref 0 in
     
     beforeEach (fun _ -> x := !x + 4);
-    test "x is 4" (fun _ -> expect !x |> toBe 4);
-    test "x is suddenly 8" (fun _ -> expect !x |> toBe 8);
+    test "x is 4" (fun _ -> if !x == 4 then pass else fail "");
+    test "x is suddenly 8" (fun _ -> if !x == 8 then pass else fail "");
   );
   
   describe "afterAll" (fun _ -> 
@@ -61,11 +60,11 @@ let _ =
     
     describe "phase 1" (fun _ ->
       afterAll (fun _ -> x := !x + 4);
-      test "x is 0" (fun _ -> expect !x |> toBe 0)
+      test "x is 0" (fun _ -> if !x == 0 then pass else fail "")
     );
     
     describe "phase 2" (fun _ -> 
-      test "x is suddenly 4" (fun _ -> expect !x |> toBe 4)
+      test "x is suddenly 4" (fun _ -> if !x == 4 then pass else fail "")
     );
   );
   
@@ -73,8 +72,8 @@ let _ =
     let x = ref 0 in
     
     afterEach (fun _ -> x := !x + 4);
-    test "x is 0" (fun _ -> expect !x |> toBe 0);
-    test "x is suddenly 4" (fun _ -> expect !x |> toBe 4);
+    test "x is 0" (fun _ -> if !x == 0 then pass else fail "");
+    test "x is suddenly 4" (fun _ -> if !x == 4 then pass else fail "");
   );
   
   describe "Only" (fun _ ->
@@ -83,21 +82,21 @@ let _ =
   );
 
   describe "Skip" (fun _ ->
-    Skip.test "Skip.test" (fun _ -> expect (1 + 2) |> toBe 3);
+    Skip.test "Skip.test" (fun _ -> pass);
 
-    Skip.testAsync "Skip.testAsync" (fun done_ ->
-      done_ (expect (1 + 2) |> toBe 3));
+    Skip.testAsync "Skip.testAsync" (fun finish ->
+      finish pass);
 
     Skip.testPromise "Skip.testPromise" (fun _ ->
-      Js.Promise.resolve (expect (1 + 2) |> toBe 3));
+      Js.Promise.resolve pass);
 
     Skip.testAll "testAll" ["foo"; "bar"; "baz"] (fun input ->
-      expect (Js.String.length input) |> toEqual 3);
+      if Js.String.length input == 3 then pass else fail "");
     Skip.testAll "testAll - tuples" [("foo", 3); ("barbaz", 6); ("bananas!", 8)] (fun (input, output) ->
-      expect (Js.String.length input) |> toEqual output);
+      if Js.String.length input == output then pass else fail "");
 
     Skip.describe "Skip.describe" (fun _ ->
-      test "some aspect" (fun _ -> expect (1 + 2) |> toBe 3)
+      test "some aspect" (fun _ -> pass);
     );
   );
   
